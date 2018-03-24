ebd8b8b4a6facdafbf3e83ed82d6566faa4cd39d
Vova M <molodiyvolodymyr@gmail.com>
2018-03-24 10:08:35 +0200

    1-changed 2-added

--- apiko/task_1/main.js
+++ apiko/task_1/main.js
@@ -1,42 +1,89 @@
 const $ = selector => document.querySelector(selector)
 
-const reloadFactory = () => ({
-    removeElements: (parentNode) => {
-      if(parentNode === null || parentNode ===undefined){
-    return
-      }
-    while(parentNode.firstChild){
-      parentNode.removeChild(parentNode.firstChild)
+const removeElements = (parentNode) => {
+    if (parentNode === null || parentNode === undefined) {
+        return
     }
-  }
-})
-const reloadInstance = Object.create(reloadFactory())
-
-const fetchUsers = async (URLreq) => {
-    try {
-        const response = await fetch(URLreq)
-        const posts = await response.json()
-        const template = await posts.map(post => `
-    <a href="index.html/posts?userId=${post.userId}">${post.userId ? ` User: ${post.userId} ` : ``}</a></br>
-    <a href= "index.html/comments?postId=${post.id}">postId: ${post.id}</a>
-    <h3>${post.title ? `Title: "${post.title}"` : ``}</h3>
-    <h3>${post.name ? `Name: "${post.name}"` : ``}</h3>
-    <p>${post.email ? `Email: "${post.email}"` : ``}</p>
-    <p>${post.body}</p>
-  `).join('')
-     $('#result').innerHTML = template
-    } catch (err) {
-        console.log(err)
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild)
     }
 }
 
-fetchUsers('https://jsonplaceholder.typicode.com/posts')
+const httpGetReq = async (URLreq) => {
+  try {
+    const response = await fetch(URLreq)
+    const posts = await response.json()
+    return posts
+   }
+    catch (err) {
+    console.log(err)
+  }
+}
+
+const mountPosts = (posts) => {
+  const template = posts.map(post => `
+<a href="index.html/posts?userId=${post.userId}" class = 'user'> ${post.userId ?'User ' + post.userId  : ''} </a></br>
+<a href="index.html/posts/${post.id}" class = 'postDetails'> ${post.id ?'Post id ' + post.id  : ''} </a></br>
+<h3> ${post.title ? 'Title: ' + post.title  : ''} </h3>
+<p>${post.body}</p>
+  `).join('')
+    $('#result').innerHTML = template
+}
+
+const mountPostsComments = (posts) => {
+    const template = posts.map(post => `
+<h5> ${post.postId ?'Post id: ' + post.postId  : ''} </h5>
+<p> ${post.id ?'Comment id: ' + post.id  : ''} </p>
+<p> ${post.name ? 'Title: ' + post.name  : ''} </p>
+<p> ${post.email ? 'Email: ' + post.email  : ''} </p>
+<p>${post.body}</p>
+     `).join('')
+        let comments = document.createElement('div')
+        comments.innerHTML = template
+        $('#result').appendChild(comments)
+}
+
+const mountPostDetails = (posts) => {
+    const {userId, id, title, body } = posts
+    const template = `
+<a href="index.html/posts?userId=${userId}" class = 'user'> ${userId ?'User ' + userId  : ''} </a></br>
+<h4>${title}</h4>
+<p>${body}</p>
+<a href= "index.html/posts/${id}/comments" class="comments"> ${id ? 'Show comments '  : ''} </a>
+`
+    $('#result').innerHTML = template
+}
+
+const mountUser =  (posts) => {
+  const template = posts.map(post => `
+<h4> ${post.userId ?'User ' + post.userId  : ''} </h4>
+<h4> ${post.id ?'Post number ' + post.id  : ''} </h4>
+<h5> ${post.title ? 'Title: ' + post.title  : ''} </h5>
+<p>${post.body}</p>
+  `).join('')
+        $('#result').innerHTML = template
+}
 
 $('#result').onclick = (e) => {
   if (e.target.tagName === 'A') {
-    const href = e.target.getAttribute('href').slice(11)
-    reloadInstance.removeElements($('#result'))
-    fetchUsers(`https://jsonplaceholder.typicode.com/${href}`)
+    const href = e.target.getAttribute('href')
+    const API_ENDPOINT = href.slice(11)
+    const httpResponse = httpGetReq(`https://jsonplaceholder.typicode.com/${API_ENDPOINT}`)
+    if (e.target.className === 'postDetails') {
+      removeElements($('#result'))
+       httpResponse.then(res => mountPostDetails(res))
+    }
+    if (e.target.className === 'user') {
+      removeElements($('#result'))
+      httpResponse.then(res => mountUser(res))
+    }
+    if (e.target.className === 'comments') {
+      httpResponse.then(res => mountPostsComments(res))
+    }
   }
     e.preventDefault()
+}
+
+window.onload = (e) => {
+   httpGetReq('https://jsonplaceholder.typicode.com/posts').then(res => mountPosts(res))
 }
---
+++ apiko/task_2/main.js
@@ -0,0 +1,86 @@
+function appendText(el, text){
+    const textNode = document.createTextNode(text)
+     el.appendChild(textNode)
+}
+
+function appendArray (el, children) {
+  children.forEach((child) =>{
+    if (Array.isArray(child)) {
+      appendArray(el, child)
+    } else if (child instanceof window.Element) {
+      el.appendChild(child)
+    } else if (typeof child === `string`) {
+      appendText(el, child)
+    }
+  })
+}
+
+function setStyles(el, styles) {
+  if (!styles) {
+    el.removeAttribute(`styles`)
+    return
+  }
+
+  Object.keys(styles).forEach((styleName) => {
+    if (styleName in el.style) {
+      el.style[styleName] = styles[styleName]
+    } else {
+      console.log(`${styleName} is not a valid style for  <${el.tagName.toLowerCase()}>`)
+    }
+  })
+}
+
+
+function createElement(type, textOrPropsOrChild, ...childrenElements) {
+  const el = document.createElement(type)
+
+  if (Array.isArray(textOrPropsOrChild)) {
+    appendArray(el, textOrPropsOrChild)
+  } else if (textOrPropsOrChild instanceof window.Element) {
+    el.appendChild(textOrPropsOrChild)
+  } else if (typeof textOrPropsOrChild === `string`) {
+    appendText(el, textOrPropsOrChild)
+  } else if (typeof textOrPropsOrChild === `object`) {
+    Object.keys(textOrPropsOrChild).forEach((propName) => {
+      if (propName in el || attributeExceptions.includes(propName)) {
+        const value = textOrPropsOrChild[propName]
+
+        if (propName === `style`) {
+          setStyles(el, value)
+        } else if (value) {
+          el[propName] = value
+        }
+      } else {
+        console.log(`${propName} is not a valid property of a <${type}>`)
+      }
+    })
+  }
+
+  if (childrenElements) appendArray(el, childrenElements)
+
+  return el
+}
+
+function render(el, parent) {
+  return parent.appendChild(el)
+}
+
+const React = {
+    createElement,
+    render
+}
+
+const app = React.createElement('div', { style: { backgroundColor: 'red' } }, [
+    React.createElement('span', undefined, 'Hello world'),
+    React.createElement('br'),
+    'This is just a text node',
+    React.createElement('div', { textContent: 'Text content' }),
+  ])
+
+React.render(
+  app,
+  document.getElementById('root'),
+)
+
+
+
